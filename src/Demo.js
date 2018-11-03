2abd39a0f8d8c249ccf02149fc076c864b536c76
Ryan Graff <ryantgraff@resistdesign.com>
2018-11-03 19:36:39 -0400

    feat: (Demo) Add random number UI.

--- src/Demo.js
+++ src/Demo.js
@@ -1,21 +1,87 @@
-import { hot } from 'react-hot-loader';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import {hot} from 'react-hot-loader';
+import React, {Component} from 'react';
+import {
+  IncarnateRouter,
+  IncarnateSwitch,
+  IncarnateRoute,
+  Incarnate,
+  LifePod
+} from './index';
 
-const Demo = class extends Component {
-  static propTypes = {};
-
-  constructor() {
-    super();
-  }
-
-  state = {};
-
+export class Demo extends Component {
   render() {
     return (
-      <h1>
-        Demo
-      </h1>
+      <IncarnateRouter
+        name='Demo'
+      >
+        <Incarnate
+          name='State'
+        >
+          <LifePod
+            name='RandomRange'
+            factory={() => 10}
+          />
+        </Incarnate>
+        <Incarnate
+          name='Utils'
+          shared={{
+            RandomRange: 'State.RandomRange'
+          }}
+        >
+          <LifePod
+            name='RandomNumber'
+            dependencies={{
+              range: 'RandomRange'
+            }}
+            factory={({dependencies: {range = 0} = {}} = {}) => Math.random() * range}
+          />
+        </Incarnate>
+        <IncarnateSwitch
+          defaultSubPath='random'
+        >
+          <IncarnateRoute
+            subPath='random'
+          >
+            <LifePod
+              dependencies={{
+                randomRange: 'State.RandomRange',
+                random: 'Utils.RandomNumber'
+              }}
+              setters={{
+                setRandomRange: 'State.RandomRange'
+              }}
+            >
+              {({
+                  dependencies: {
+                    randomRange = '',
+                    random = 0
+                  },
+                  setters: {
+                    setRandomRange
+                  }
+                } = {}) => (
+                <div>
+                  Random Number: {random}
+                  <br/>
+                  <br/>
+                  Random Range:
+                  <br/>
+                  <input
+                    type='number'
+                    value={randomRange}
+                    onChange={({target: {value} = {}} = {}) => setRandomRange(parseFloat(value))}
+                  />
+                </div>
+              )}
+            </LifePod>
+          </IncarnateRoute>
+          <IncarnateRoute
+            subPath='multiply/:x/:y'
+          >
+
+          </IncarnateRoute>
+        </IncarnateSwitch>
+      </IncarnateRouter>
     );
   }
 }
