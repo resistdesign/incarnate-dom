e248423b9e30e7fc4f8c282ef9c842ee36020179
Ryan Graff <ryantgraff@resistdesign.com>
2018-11-04 21:00:19 -0500

    feat: (index) Add Traverse.

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import MemoizeInternal from './Control/Memoize';
+import TraverseInternal from './Control/Traverse';
 import IncarnateRouterInternal from './Routing/IncarnateRouter';
 import IncarnateRouteInternal from './Routing/IncarnateRoute';
 import IncarnateSwitchInternal from './Routing/IncarnateSwitch';
@@ -7,6 +8,8 @@ import LifePodInternal from './LifePod';
 import IncarnateInternal from './Incarnate';
 
 export const Memoize = MemoizeInternal;
+
+export const Traverse = TraverseInternal;
 
 export const IncarnateRouter = IncarnateRouterInternal;
 
