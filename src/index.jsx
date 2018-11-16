1dd1078062a9be6e12287236439316d399a76849
Ryan Graff <ryan.graff@bedrockdata.com>
2018-11-16 13:35:23 -0500

    refactor: Remove Monitoring tools from main export to support minimal bundle sizes.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import CollectionInternal from './Control/Collection';
 import MemoizeInternal from './Control/Memoize';
 import TraverseInternal from './Control/Traverse';
-import TreeInternal from './Monitoring/Tree';
 import IncarnateRouterInternal from './Routing/IncarnateRouter';
 import IncarnateRouteInternal from './Routing/IncarnateRoute';
 import IncarnateSwitchInternal from './Routing/IncarnateSwitch';
@@ -14,8 +13,6 @@ export const Collection = CollectionInternal;
 export const Memoize = MemoizeInternal;
 
 export const Traverse = TraverseInternal;
-
-export const Tree = TreeInternal;
 
 export const IncarnateRouter = IncarnateRouterInternal;
 
